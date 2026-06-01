450fa30ebe6c05dde0da8d4edc7b96898eba2333
jaxson17 <jaesondiaz4@gmail.com>
2026-05-31 20:45:43 -0600

    Create condicionales2.py

---
+++ condicionales2.py
@@ -0,0 +1,13 @@
+print("Escribe tu nombre:")
+nombre = input()
+
+#elif 
+
+
+if nombre == "Antonio":
+    print("Saludo Antonio")
+elif nombre == "Carlos":
+    print("Que bonito nombre Carlos")
+
+else:
+    print("Que extraño nombre")
